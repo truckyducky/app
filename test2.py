fd197157f2c8d80ddd8f1a07e55bb503beb20f73
Truc <132505670+truckyducky@users.noreply.github.com>
2023-09-05 17:26:15 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -146,4 +146,4 @@ except:
     HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
 # Load HTML file in HTML component for display on Streamlit page
-components.html(HtmlFile.read(), height=800)
+components.html(HtmlFile.read(), height=800, width = 800)
