83b212c5a62eeee213a8c77a2b012f24bf4a2c3e
Truc <132505670+truckyducky@users.noreply.github.com>
2023-09-05 17:26:54 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,7 +6,7 @@ from pyvis.network import Network
 
 
 #setup network
-net = Network(notebook = True, cdn_resources="remote",height="900px", width="90%", select_menu = True, filter_menu=True,font_color="white", bgcolor = "#373a3c", heading = "Interactive Network of HEAL Core CDEs")
+net = Network(notebook = True, cdn_resources="remote",height="900px", width="100%", select_menu = True, filter_menu=True,font_color="white", bgcolor = "#373a3c", heading = "Interactive Network of HEAL Core CDEs")
 
 
 #add categorical/subcategorical nodes
