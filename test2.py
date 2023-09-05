e6e9997eb725184379cea73298846cb8fe16d9a4
Truc <132505670+truckyducky@users.noreply.github.com>
2023-09-05 18:13:30 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -7,10 +7,7 @@ from pyvis.network import Network
 
 
 #setup network
-st.title("Interactive Network of HEAL Core CDEs")
-def do_stuff_on_page_load(...):
-    st.set_page_config(layout="wide")
-    
+st.title("Interactive Network of HEAL Core CDEs")  
 net = Network(notebook = True, cdn_resources="remote",height="800px", width="100%", select_menu = True, filter_menu=True,font_color="white", bgcolor = "#373a3c")
 
 
