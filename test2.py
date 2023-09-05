6055db849c0d22392118c5cf2735d8d292be590a
Truc <132505670+truckyducky@users.noreply.github.com>
2023-09-05 16:55:20 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,10 +1,7 @@
 #setting up packages
 import streamlit as st
 import streamlit.components.v1 as components
-import pandas as pd
 import networkx as nx
-import datapane as dp
-from stvis import pv_static
 from pyvis.network import Network
 from IPython.core.display import display, HTML
 
