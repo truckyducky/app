ca4f138d268dac2fbc2732fbf23f3cb5bf4c12f5
Truc <132505670+truckyducky@users.noreply.github.com>
2023-09-05 17:04:32 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -100,13 +100,13 @@ net.show("edges.html")
 # Save and read graph as HTML file (on Streamlit Sharing)
 try:
     path = '/tmp'
-    drug_net.save_graph(f'{path}/pyvis_graph.html')
+    net.save_graph(f'{path}/pyvis_graph.html')
     HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
 # Save and read graph as HTML file (locally)
 except:
     path = '/html_files'
-    drug_net.save_graph(f'{path}/pyvis_graph.html')
+    net.save_graph(f'{path}/pyvis_graph.html')
     HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
 # Load HTML file in HTML component for display on Streamlit page
