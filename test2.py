2538e2445275035fabff4e667f8341653501f5d1
Truc <132505670+truckyducky@users.noreply.github.com>
2023-10-14 14:39:16 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1545,6 +1545,117 @@ net.add_edge('Node 35 - Tobacco, Alcohol, Prescription Medication, and Other Sub
 net.add_edge('Node 35 - Tobacco, Alcohol, Prescription Medication, and Other Substance Use Tool [TAPS-1]','Node 125 - Mapping the joint-nerve interactome of the knee')
 net.add_edge('Node 35 - Tobacco, Alcohol, Prescription Medication, and Other Substance Use Tool [TAPS-1]','Node 126 - Neural architecture of the murine and human temporomandibular joint')
 
+# connecting study names to their respective research programs
+
+net.add_edge('Node 36 - Biomarkers','Node 50 - SPRINT: Signature for Pain Recovery IN Teens')
+net.add_edge('Node 36 - Biomarkers','Node 57 - Biomarker Signature to Predict the Persistence of Post-Traumatic Headache')  
+net.add_edge('Node 36 - Biomarkers','Node 58 - Discovery and Analytical Validation of Inflammatory Bio-Signatures of the Human Pain Experience')
+net.add_edge('Node 36 - Biomarkers','Node 59 - Multi-Omic Biomarkers for Neuropathic Pain Secondary to Chemotherapy')
+net.add_edge('Node 36 - Biomarkers','Node 60 - Validation of a novel cortical biomarker signature for pain')
+net.add_edge('Node 36 - Biomarkers','Node 84 - Discovery of Biomarker Signatures Prognostic for Neuropathic Pain after Acute Spinal Cord Injury')
+net.add_edge('Node 36 - Biomarkers','Node 85 - Discovery of the Biomarker Signature for Neuropathic Corneal Pain')
+net.add_edge('Node 36 - Biomarkers','Node 86 - Mentoring in Discovery and Validation of Clinical Chronic Pain Biomarkers')
+
+
+              
+net.add_edge('Node 37 - ERN','Node 51 - SurgeryPal')
+net.add_edge('Node 37 - ERN','Node 61 - Pain Response Evaluation of a Combined Intervention to Cope Effectively (PRECICE)')
+net.add_edge('Node 37 - ERN','Node 90 - A sequenced-strategy for improving outcomes in patients with knee osteoarthritis pain')
+net.add_edge('Node 37 - ERN', 'Node 91 - Integrated Treatment for Veterans with Co-Occurring Chronic Pain and Opioid Use Disorder')
+net.add_edge('Node 37 - ERN', 'Node 92 - Optimizing the use of ketamine to reduce chronic postsurgical pain')
+net.add_edge('Node 37 - ERN', 'Node 93 - Pain Response Evaluation of a Combined Intervention to Cope Effectively (PRECICE)')
+net.add_edge('Node 37 - ERN', 'Node 94 - RESOLVE- Tailored Non-Pharmacotherapy Services for Chronic Pain: Testing Scalable and Pragmatic Approaches')
+net.add_edge('Node 37 - ERN', 'Node 95 - Wake Forest NCORP Research Base')
+              
+              
+              
+net.add_edge('Node 38 - Health Equity in Pain Management','Node 62 - Achieving Equity through SocioCulturally-informed, Digitally-Enabled Cancer Pain managemeNT" (ASCENT) Clinical Trial')
+net.add_edge('Node 38 - Health Equity in Pain Management','Node 52 - Culturally adapted mobile treatment of chronic pain in adolescent survivors of pediatric bone sarcoma') 
+net.add_edge('Node 38 - Health Equity in Pain Management','Node 53 - Integrative Training Program for Pediatric Sickle Cell Pain')
+net.add_edge('Node 38 - Health Equity in Pain Management','Node 54 - Latinx Children and Surgery')
+net.add_edge('Node 38 - Health Equity in Pain Management','Node 65 - PAINED: Project Addressing INequities in the Emergency Department')
+net.add_edge('Node 38 - Health Equity in Pain Management','Node 96 - Addressing the chronic pain epidemic among older adults in underserved community center; The GetActive+ study')
+net.add_edge('Node 38 - Health Equity in Pain Management','Node 97 - Equity Using Interventions for Pain and Depression (EQUIPD)')
+net.add_edge('Node 38 - Health Equity in Pain Management','Node 98 - Group-based Integrative Pain Management: A multi-level approach to address intersectional stigma and social isolation in diverse primary care safety net patients with chronic pain')
+net.add_edge('Node 38 - Health Equity in Pain Management','Node 99 - Integrating Nonpharmacologic Strategies for Pain with Inclusion, Respect, and Equity (INSPIRE): Tailored digital tools, telehealth coaching, and primary care coordination')
+net.add_edge('Node 38 - Health Equity in Pain Management','Node 100 - Partners for Pain & Wellbeing Equity: A Randomized Trial of Community Supported Complementary and Integrative Health Self Management for Back Pain')
+              
+net.add_edge('Node 39 - A2CPS','Node 55 - Transition from Acute to Chronic Pain After Thoracic Surgery')
+              
+              
+net.add_edge('Node 40 - BACPAC','Node 56 - Development, Evaluation and Translation of Robotic Apparel for Alleviating Low Back Pain')
+net.add_edge('Node 40 - BACPAC','Node 72 - COMEBACK')
+net.add_edge('Node 40 - BACPAC','Node 73 - Focused Ultrasound Neuromodulation of Dorsal Root Ganglion for Noninvasive Mitigation of Low Back Pain')
+net.add_edge('Node 40 - BACPAC','Node 74 - HEALing LB3P: Profiling Biomechanical, Biological and Behavioral Phenotypes')
+net.add_edge('Node 40 - BACPAC','Node 75 - Imaging Epigenetic Dysregulation in Patients with Low Back Pain')
+net.add_edge('Node 40 - BACPAC','Node 76 - Nonpharmacologic Pain Management for Lumbar Surgery')
+net.add_edge('Node 40 - BACPAC','Node 77 - Novel Imaging of Endplate Biomarkers in Chronic Low Back Pain')
+net.add_edge('Node 40 - BACPAC','Node 78 - Proof of Concept Study to Treat Negative Affect in Chronic Low Back Pain')
+net.add_edge('Node 40 - BACPAC','Node 79 - Randomized-controlled trial of virtual reality for chronic lower back pain to improve patient-reported outcomes and physical activity')
+net.add_edge('Node 40 - BACPAC','Node 80 - Technology Research Site for Advanced, Faster Quantitative Imaging for BACPAC')
+net.add_edge('Node 40 - BACPAC','Node 81 - The Spine Phenome Project: Enabling Technology for Personalized Medicine')
+net.add_edge('Node 40 - BACPAC','Node 82 - UM MRC BACPAC')
+net.add_edge('Node 40 - BACPAC','Node 83 - Wearable nanocomposite sensor system for diagnosing mechanical sources of low back pain and guiding rehabilitation')
+              
+
+              
+net.add_edge('Node 41 - IMPOWR','Node 66 - Implementation and Effectiveness of Mindfulness Oriented Recovery Enhancement as an Adjunct to Methadone Treatment for Opioid Use Disorder')
+net.add_edge('Node 41 - IMPOWR','Node 102 - Healing Opioid Misuse and Pain Through Engagement (HOPE) Trial')
+net.add_edge('Node 41 - IMPOWR','Node 103 - Intergrated Care for Chronic Pain and Opioid Use Disorder: The IMPOWR Research Center at Montefiore/Einstein (IMPOWR-ME)')
+net.add_edge('Node 41 - IMPOWR','Node 104 - Pain Care At Home to Amplify Function (Pain CHAMP)')
+net.add_edge('Node 41 - IMPOWR','Node 105 - Randomized Clinical Trial Intervention to Treat Chronic Pain Among Persons Maintained on Methadone for Opioid Use Disorder')
+net.add_edge('Node 41 - IMPOWR','Node 106 - Stepped Care for Patients to Optimize Whole Recovery (SC-POWR)')
+net.add_edge('Node 41 - IMPOWR','Node 107 - Tailored Retention and Engagement for Equitable Treatment of OUD and Pain (TREETOP)')
+              
+
+
+net.add_edge('Node 42 - Interdisciplinary Teams to Elucidate the Mechanisms of Device-Based Pain Relief', 'Node 67 - RM1 Project 1: Evaluating the specific role of endogenous opioids as the mechanism underlying tAN-based analgesia in healthy individuals')
+net.add_edge('Node 42 - Interdisciplinary Teams to Elucidate the Mechanisms of Device-Based Pain Relief','Node 68 - RM1 Project 2: Determining the independent and synergistic effects of transcutaneous auricular neurostimulation (tAN) on direct brain activation in healthy individuals')
+net.add_edge('Node 42 - Interdisciplinary Teams to Elucidate the Mechanisms of Device-Based Pain Relief','Node 108 - From Nerve to Brain: Toward a Mechanistic Understanding of Spinal Cord Stimulation in Human Subjects')
+net.add_edge('Node 42 - Interdisciplinary Teams to Elucidate the Mechanisms of Device-Based Pain Relief','Node 109 - Understanding the Mechanistic, Neurophysiological, and Antinociceptive Effects of Transcutaneous Auricular Neurostimulation for Treatment of Chronic Pain')
+              
+              
+net.add_edge('Node 43 - MPS- Myofascial Pain','Node 69 - HEAL Initiative: Developing Quantitative Imaging and Other Relevant Biomarkers of Myofascial Tissues for Clinical Pain Management')
+net.add_edge('Node 43 - MPS- Myofascial Pain','Node 110 - Development and identification of magnetic resonance, electrophysiological, and fiber-optic imaging biomarkers of myofascial pain')
+net.add_edge('Node 43 - MPS- Myofascial Pain','Node 111 - Development and Validation of a Noninvasive Multimodal Ultrasound-Based Imaging Biomarker for Myofascial Pain')
+net.add_edge('Node 43 - MPS- Myofascial Pain','Node 112 - MRI-based quantitative characterization of impaired myofascial interface properties in myofascial pain syndrome')
+net.add_edge('Node 43 - MPS- Myofascial Pain','Node 113 - Multimodal imaging biomarkers for investigating fascia, muscle and vasculature in myofascial pain')
+net.add_edge('Node 43 - MPS- Myofascial Pain','Node 114 - Quantifying and Treating Myofascial Dysfunction in Post Stroke Shoulder Pain')
+net.add_edge('Node 43 - MPS- Myofascial Pain','Node 115 - Total-body PET for assessing myofascial pain')
+              
+              
+              
+              
+net.add_edge('Node 44 - PRECISION Human Pain', 'Node 70 - INTERCEPT: Integrated Research Center for human Pain Tissues')
+net.add_edge('Node 44 - PRECISION Human Pain','Node 116 - Harvard PRECISION Human Pain Center')
+net.add_edge('Node 44 - PRECISION Human Pain','Node 117 - Human Nociceptor and Spinal Cord Molecular Signature Center')
+              
+              
+net.add_edge('Node 45 - PRISM', 'Node 71 - Fibromyalgia TENS in Physical Therapy Study (TIPS):an embedded pragmatic clinical trial')
+net.add_edge('Node 45 - PRISM','Node 118 - Group-Based Mindfulness for Patients with Chronic Low Back Pain in the Primary Care')
+net.add_edge('Node 45 - PRISM','Node 119 - Hybrid Effectiveness-Implementation Trial of Guided Relaxation and Acupuncture for Chronic Sickle Cell Disease Pain')
+net.add_edge('Node 45 - PRISM','Node 120 - Nonpharmacologic Pain Management for Lumbar Surgery')
+net.add_edge('Node 45 - PRISM','Node 121 - Non-pharmacological Options in postoperative Hospital-based And Rehabilitation pain Management (NOHARM) pragmatic clinical trial')
+net.add_edge('Node 45 - PRISM','Node 122 - Pragmatic Trial of Acupuncture for Chronic Low Back Pain in Older Adult')
+              
+              
+              
+net.add_edge('Node 46 - HOPE', 'Node 101 - A Randomized Clinical Trial to Evaluate Non-Pharmacologic and Pharmacologic Approaches for Reducing Pain and Opioid Use Among Patients Treated with Maintenance Hemodialysis')
+
+              
+              
+net.add_edge('Node 47 - EPPIC Net', 'Node 87 - A Study to Evaluate the Safety and Efficacy of CNTX-6970 in Subjects With Knee Osteoarthritis Pain')
+net.add_edge('Node 47 - EPPIC Net','Node 88 - EPPIC-Net: Novaremed Painful Diabetic Peripheral Neuropathy ISA (EN21-01)')
+net.add_edge('Node 47 - EPPIC Net','Node 89 - EPPIC-Net: Platform Protocol to Assess Treatments for Painful Diabetic Peripheral Neuropathy')
+              
+              
+net.add_edge('Node 48 - RE JOIN','Node 123 - Comprehensive functional phenotyping of trigeminal neurons innervating temporomandibular joint (TMJ) tissues in male, female and aged mice, primates, and humans with and without TMJ disorders (TMJD)')
+net.add_edge('Node 48 - RE JOIN','Node 124 - Innervation of the knee and TMJ')
+net.add_edge('Node 48 - RE JOIN','Node 125 - Mapping the joint-nerve interactome of the knee')
+net.add_edge('Node 48 - RE JOIN','Node 126 - Neural architecture of the murine and human temporomandibular joint')
+
+
+
 
 net.repulsion(spring_strength = 0)
 net.show("edges.html")
